736b9395799d2e48e976807abffe87c3dbfe74b6
Julian Hildebrandt <julianhildebrandt@072-117.eduroam.rwth-aachen.de>
2018-10-17 17:58:27 +0200

    Beispiel

---
+++ 00_packages.R
@@ -0,0 +1,2 @@
+#Gapminder Datensatz
+install.packages("gapminder")
---
+++ AnalyseScript.R
@@ -0,0 +1,12 @@
+# Analyse Script
+
+source("01_dataloading.R")
+
+library(haven)
+library(tidyverse)
+
+
+t.test(anscombe$x1)
+
+
+# --- TEst 
